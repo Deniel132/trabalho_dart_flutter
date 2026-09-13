f5ccd1b6ba247a0faa9333cf1a026f7f5b872368
T1AGO55 <tiagotsh1@gmail.com>
2026-09-13 02:47:40 -0300

    Refatorando Model e Criando widget de Listagem

--- part1_dart/bin/models/pc.dart
+++ part1_dart/bin/models/pc.dart
@@ -4,25 +4,25 @@ import './games.dart';
 class Pc extends Hardware {
   final DateTime _dataFabricacao;
   final double _valor;
-  final double _benchmark;
   final List<Games> _jogosinstalados = [];
-  
+
   Pc({
     required super.processador,
     required super.qtdMemoriaRam,
     required super.placaDeVideo,
     required super.armazenamento,
     required super.tipoArmazenamento,
+    required super.benchmark,
+    required super.status,
+    required super.valorManutencao,
     required DateTime dataFabricacao,
     required double valor,
-    required double benchmark,
   }) : _dataFabricacao = dataFabricacao,
-       _valor = valor,
-       _benchmark = benchmark;
+       _valor = valor;
 
   @override
   String fichaTecnica() {
-    return "Processador: ${getProcessador()} | Memoria Ram: ${getqtdMemoriaRam}gb | Placa de Video: $getPlacaDeVideo | Armazenamento: $getArmazenamento | Tipo Armazenamento: ${getTipoArmazenamento.name}| BenchMark: $_benchmark | Data Fabricacao: $_dataFabricacao | Valor: $_valor";
+    return "Processador: ${getProcessador()} | Memoria Ram: ${getqtdMemoriaRam}gb | Placa de Video: $getPlacaDeVideo | Armazenamento: $getArmazenamento | Tipo Armazenamento: ${getTipoArmazenamento.name} | Benchmark: ${getBenchmark} | Status: ${getStatus} | Valor da Manutenção: ${getValorManutencao} | Data Fabricacao: $_dataFabricacao | Valor: $_valor";
   }
 
   int getTotalInstalado() {
@@ -39,7 +39,7 @@ class Pc extends Hardware {
   }
 
   void intallGame(Games games) {
-    if (games.getBenchMark() > _benchmark) {
+    if (games.getBenchMark() > getBenchmark) {
       print(
         "\nRequisitos Minimos nâo atendidos! Possíveis problemas ao executar!\n",
       );
@@ -52,6 +52,4 @@ class Pc extends Hardware {
       print(jogo.getNome());
     }
   }
-
-
 }
---
+++ parte2_flutter/lib/models/Manutencao.dart
@@ -0,0 +1,19 @@
+import './hardware.dart';
+class Manutencao {
+  final List<Hardware> _hardwares = [];
+
+  void enviarParaManutencao(Hardware hardware){
+    print("Enviado hardwares para Manutenção: ${hardware.fichaTecnica()} ");
+    _hardwares.add(hardware);
+  }
+
+  int get getTotalHardware => _hardwares.length;
+
+  void mostrarHardware(){
+    for(var hardware in _hardwares){
+      print(" - ${hardware.getProcessador()}" );
+    }
+  }
+
+
+}
