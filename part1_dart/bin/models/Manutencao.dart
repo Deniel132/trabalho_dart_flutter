2f1faa5aa500487ae537088b6d6223dafe5c931f
T1AGO55 <tiagotsh1@gmail.com>
2026-09-14 01:23:46 -0300

    Alteracao no Hardware Nome da funcao adicionarManutencao

--- part1_dart/bin/models/Manutencao.dart
+++ part1_dart/bin/models/Manutencao.dart
@@ -1,29 +1,32 @@
 import './hardware.dart';
+
 class Manutencao {
   final List<Hardware> _hardwares = [];
 
-  void enviarParaManutencao(Hardware hardware){
+  void adicionarManutencao(Hardware hardware) {
     print("Enviado hardwares para Manutenção: ${hardware.fichaTecnica()} ");
     _hardwares.add(hardware);
   }
 
   int get getTotalHardware => _hardwares.length;
 
-  void mostrarHardware(){
-    for(var hardware in _hardwares){
-      print(" - ${hardware.getProcessador()}" );
+  void mostrarHardware() {
+    for (var hardware in _hardwares) {
+      print(" - ${hardware.getProcessador()}");
     }
   }
 
-  List<Hardware> getByStatus(List<status> status){
-    return _hardwares.where((hardware) => status.contains(hardware.getStatus)).toList();
+  List<Hardware> getByStatus(List<status> status) {
+    return _hardwares
+        .where((hardware) => status.contains(hardware.getStatus))
+        .toList();
   }
 
-  Hardware getByIndex(int index){
+  Hardware getByIndex(int index) {
     return _hardwares[index];
   }
 
-  double getValorTotal(){
+  double getValorTotal() {
     double valorTotal = 0;
     for (var hardware in _hardwares) {
       valorTotal += hardware.getValorManutencao;
